0decee94c7401db8497f71eee2b46e89356d1813
Matthew Gerry <mgerry04@gmail.com>
2023-02-21 14:47:25 -0500

    Created matlab script for the case with bias

---
+++ modular_RW_bias.m
@@ -0,0 +1,11 @@
+% modular_RW_bias.m
+
+% Statistcs for a classical random walk with transition rates based on a
+% quantum mechanical model with tunnelling coupling between sites on a
+% molecule/chain and local decoherence. Decoherence rates alternate in
+% modules of sizes nA and nB (specified). Here we investigate the case with
+% bias (constant ratio of forward to reverse rates passed to the function
+% diffusionLchi which builds the counting field-dressed Liouvillian).
+
+% Matthew Gerry, February 2023
+

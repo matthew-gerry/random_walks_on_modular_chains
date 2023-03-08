5b8c4e31606ef5ffd2f1bd901595e92083774ede
Matthew Gerry <mgerry04@gmail.com>
2023-03-08 16:00:25 -0500

    Re-working the CGF_symb script to look at expansions of the CGF in different variables.

--- modular_RW_CGF_symb.m
+++ modular_RW_CGF_symb.m
@@ -1,29 +1,28 @@
 % modular_RW_CGF.m
 
-% THIS SCRIPT IS NOT WORKING AS DESIRED - CALLS diffusionCGF_symb FUNCTION
-% WHICH COULD NOT PROPERLY GET AN EXPRESSION FOR THE CGF AND THUS HAS SINCE
-% BEEN MODIFIED AND RENAMED
-
 % Investigating the properties of the cumulant generating function for the
 % infinite (equivalent cycle) modular random walk. Particularly we are
 % interested in the impact of bias and block_length on the CGF and
 % associated cumulants.
 
-% Matthew Gerry, February 2023
+% We expand the CGF in chi, and then expand each of the coefficient in
+% powers of the variables of interest: delta_gamma and b.
+
+% Matthew Gerry, March 2023
 
 %%% CASE 1: NO BIAS
 
 % Numerical values for parameters
-tau_val = 1;
-ga_av_val = 10;
-dga_val = 5;
-b_val = 1; % no bias
-
-n_max = 3;
-n_list = 1:n_max; % List of blocks lengths (fix two regions equal in size)
-chi_list = 0:0.01:5; % For plotting the CGF
-
-% Allocate matrices to hold values of CGF
+% tau_val = 1;
+% ga_av_val = 10;
+% dga_val = 5;
+% b_val = 1; % no bias
+% 
+% n_max = 3;
+% n_list = 1:n_max; % List of blocks lengths (fix two regions equal in size)
+% chi_list = 0:0.01:5; % For plotting the CGF
+% 
+% % Allocate matrices to hold values of CGF
 % G_re_vals = zeros(n_max, length(chi_list));
 % G_im_vals = zeros(n_max, length(chi_list));
 
@@ -59,9 +58,28 @@ chi_list = 0:0.01:5; % For plotting the CGF
 
 %%% ANALYTIC EXPRESSIONS FOR CUMULANTS, WITH BIAS
 n = 2; % For now
-[tau, ga_av, dga, b, chi, G] = diffusionCGF_symb(n,n);
+[tau, ga_av, dga, lr, chi, Lchi] = diffusionLchi_symb(n,n);
 
-J = subs(diff(G,chi),chi,0);
-S = subs(diff(G,chi,2),chi,0);
+Lchi_eigs = eig(Lchi); % Eigenvalues of the chi-dressed rate matrix
+
+% Verify which eigenvalue is the scaled CGF by plugging in chi=0 and
+% identifying the one that vanishes - note there is discontinuous behavour
+% here at zero bias - it appears to be the first element of Lchi_eigs for
+% arbitrarily small but finite bias, but then its the second element at
+% exactly zero bias
+Lchi_mixed = simplify(subs(Lchi, [tau, ga_av, dga, lr, chi], [1, 10, 5, 0.00001, 0]));
+mixed_eigs = double(eig(Lchi_mixed));
+CGF_index = find(mixed_eigs==0);
+
+CGF = Lchi_eigs(CGF_index);
+
+% Let's begin the expansions
+% Mean current
+J = subs(diff(CGF, chi), chi, 0); % Symbolic function for the current
+
+
+
 
 % SO FAR, NOT WORKING
+
+

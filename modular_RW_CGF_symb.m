93177ef1ef0c2dd45c0f5d280712a8a0704fc23e
Matthew Gerry <mgerry04@gmail.com>
2023-03-08 16:47:23 -0500

    Working on symbolic code - so far it's too slow

--- modular_RW_CGF_symb.m
+++ modular_RW_CGF_symb.m
@@ -10,55 +10,11 @@
 
 % Matthew Gerry, March 2023
 
-%%% CASE 1: NO BIAS
-
-% Numerical values for parameters
-% tau_val = 1;
-% ga_av_val = 10;
-% dga_val = 5;
-% b_val = 1; % no bias
-% 
-% n_max = 3;
-% n_list = 1:n_max; % List of blocks lengths (fix two regions equal in size)
-% chi_list = 0:0.01:5; % For plotting the CGF
-% 
-% % Allocate matrices to hold values of CGF
-% G_re_vals = zeros(n_max, length(chi_list));
-% G_im_vals = zeros(n_max, length(chi_list));
-
-%%% JUST PLOT THE CGF NUMERICALLY, THERE IS NO NEED TO USE SYMBOLIC MANIPULATION
-
-% syms tau ga_av dga b chi
-
-% for n=n_list
-%     [tau, ga_av, dga, b, chi, G] = diffusionCGF_symb(n,n);
-% 
-%     % Substitute numerical values
-%     G_plot = subs(G, [tau, ga_av, dga, b], [tau_val, ga_av_val, dga_val, b_val]);
-%     G_plot = double(subs(G_plot,chi,chi_list));
-%     
-%     % Record values
-%     G_re_vals(n,:) = real(G_plot);
-%     G_im_vals(n,:) = imag(G_plot);
-% end % n
-%     
-% figure
-% subplot(1,2,1); hold on; box on
-% for n=n_list
-%     plot(chi_list, G_im_vals(n,:))
-% end
-% hold off
-% 
-% subplot(1,2,2); hold on; box on
-% for n=n_list
-%     plot(chi_list, G_re_vals(n,:))
-% end
-% hold off
-
-
 %%% ANALYTIC EXPRESSIONS FOR CUMULANTS, WITH BIAS
+
 n = 2; % For now
 [tau, ga_av, dga, b, chi, Lchi] = diffusionLchi_symb(n,n);
+Lchi = subs(Lchi, [tau, ga_av], [1,10]); % Substitute some numerical values to speed up code
 
 Lchi_eigs = eig(Lchi); % Eigenvalues of the chi-dressed rate matrix
 
@@ -67,19 +23,20 @@ Lchi_eigs = eig(Lchi); % Eigenvalues of the chi-dressed rate matrix
 % here at zero bias - it appears to be the first element of Lchi_eigs for
 % arbitrarily small but finite bias, but then its the second element at
 % exactly zero bias
-Lchi_mixed = simplify(subs(Lchi, [tau, ga_av, dga, b, chi], [1, 10, 5, 0.00001, 0]));
-mixed_eigs = double(eig(Lchi_mixed));
+Lchi_mixed = simplify(subs(Lchi, chi, 0));
+mixed_eigs = eig(Lchi_mixed);
 CGF_index = find(mixed_eigs==0);
 
 CGF = Lchi_eigs(CGF_index);
 
 % Let's begin the expansions
 % Mean current
-J = subs(diff(CGF, chi), chi, 0); % Symbolic function for the current
+J = -1i*subs(diff(CGF, chi), chi, 0); % Symbolic function for the current
 
+% Mean current dependence on delta_gamma
+J_dga_1 = diff(J,dga);
+J_dga_2 = diff(J_dga_1, dga);
 
-
-
-% SO FAR, NOT WORKING
+% SO FAR, TOO SLOW
 
 

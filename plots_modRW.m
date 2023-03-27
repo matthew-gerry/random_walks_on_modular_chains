aa1276c4daa5d57245731f61304ed14df4a31aea
Matthew Gerry <mgerry04@gmail.com>
2023-03-27 10:14:28 -0400

    Created matlab script to generate plots

---
+++ plots_modRW.m
@@ -0,0 +1,7 @@
+% plots_modRW.m
+
+% Creation of the plots to be used in the infinite modular random walk
+% project. The four cumulants with varying bias at varying block_lengths
+
+% Matthew Gerry, March 2023
+

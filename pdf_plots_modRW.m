338d7a0cec4e0c6a0ce0a932e010affb08776054
Matthew Gerry <mgerry04@gmail.com>
2023-04-21 16:35:12 -0400

    pdf plotting function: cleaned up and used to get pdfs at high bias

--- pdf_plots_modRW.m
+++ pdf_plots_modRW.m
@@ -7,17 +7,17 @@
 % Matthew Gerry, April 2023
 
 %%% PARAMETERS %%%
-tau = 1.0; % 1/s, tunneling element
+tau = 1; % 1/s, tunneling element
 ga_av = 1.0; % 1/s, average decoherence rate
 dga = 1.0; % 1/s, difference between decoherence rates
 
-b = 0.2; % Bias
+b = 8; % Bias
 m_list = [1,2,4,8]; % Segment length (even segments)
 
 % Simulation parameters
-numsites = 161; % Number of sites
-dt = 0.1; % s, time step
-tmax = 120; % s, max time
+numsites = 281; % Number of sites
+dt = 0.2; % s, time step
+tmax = 80; % s, max time
 time = 0:dt:tmax; % time array
 
 
@@ -32,11 +32,12 @@ big_C3 = zeros(length(time),length(m_list));
 big_C4 = zeros(length(time),length(m_list));
 
 % Get results for analogous homogeneous walk (dga=0)
-[PDF_hom, sites, n_av_hom, ~, D_av_hom, C3_hom, C4_hom] = pdf_direct(2,2,b,ga_av,0,tau,numsites,dt,tmax);
+[PDF_hom, sites, n_av_hom, ~, D_av_hom, C3_hom, C4_hom] = pdf_direct(2,2,b,ga_av,0.0,tau,numsites,dt,tmax);
 
 for ii=1:length(m_list)
     m = m_list(ii);
 
+    % Derive probability distributions and cumulants using PDF_direct function
     [PDF, ~, n_av, ~, D_av, C3, C4] = pdf_direct(m,m,b,ga_av,dga,tau,numsites,dt,tmax);
 
     bigPDF(:,:,ii) = PDF;
@@ -49,8 +50,7 @@ end
 
 %%% PLOT PROBABILITY DISTRIBUTIONS %%%
 
-% snapshot_times = floor((tmax/dt)*[0.25, 0.6, 0.99]); % Specific indices of time array at which to show PDF
-snapshot_times = [100, 350, 800];
+snapshot_times = [50, 175, 400];  % Specific indices of time array at which to show PDF (arb.)
 
 colourlist = ["#0072BD", "#D95319", "#77AC30","#7E2F8E"];
 lettlist = ["(a) ", "(b) ", "(c) ", "(d) "];
@@ -66,7 +66,8 @@ for ii=1:length(m_list)
         bar(sites, bigPDF(:,t_snap,ii), facecolor=colourlist(jj), facealpha=0.6, EdgeColor="none",DisplayName=strcat("$t=\;$",num2str(dt*t_snap),"$\;s$"));
         hom_curve.Annotation.LegendInformation.IconDisplayStyle = "off";
     end % jj
-    ylim([0, 1.2*max(PDF_hom(:,snapshot_times(1)))])
+%     xlim([-10,sites(end)]) % If high bias
+    ylim([0, 1.2*max(bigPDF(:,snapshot_times(1),ii))])
     yl = ylim; xl = xlim;
     if ii>2; xlabel("$n$",Interpreter="latex"); end
     if rem(ii,2)~=0; ylabel("$P_n$",Interpreter="latex"); end
@@ -92,7 +93,7 @@ legend(Interpreter="latex", Location="southeast")
 set(gca,fontsize=14)
 hold off
 
-% Diffusion coefficient
+% Diffusion coefficient times 2 (scaled variance)
 subplot(2,2,2); box on; hold on;
 plot(time, 2*D_av_hom, '--k')
 for ii=1:length(m_list)
